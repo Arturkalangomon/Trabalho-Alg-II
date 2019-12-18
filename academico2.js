a9cd592c4a31f7747c1a8dec4cf5d0ea9957c246
Arturkalangomon <39973544+Arturkalangomon@users.noreply.github.com>
2019-12-18 20:31:28 -0300

    Sistema de Notas Alunos
    Trabalho de Alg II 
    Grupo 1 (Sistema de Cadastro de Aluno (notas) simples)
    Artur, Joyce Maria, Amarildo, Roseane Felix Matias
    Arquivo trabalhoacad.js (arquivo do sistema trabalho)
    Arquivo Academico2.js (prototipo incompleto (testes))

---
+++ academico2.js
@@ -0,0 +1,265 @@
+//copia para testes de implementação academico.js
+//programa principal... modificado...
+//importações...
+const terminal = require('./../terminal/terminal.js');
+const strings = require('./../strings/strings.js');
+const colegio = require('./../colegio/colegio.js');
+//const processamenus = require('./../arquivo/processamenus.js');
+
+
+//arquivo (nome.extensão)... se não existe, vai dar erro...
+const fs = require('fs'); //manipula arquivos... 
+const localArquivo = './../arquivo/teste.denylson'; //especifica o local e o arquivo...
+
+const bd = fs.readFileSync(localArquivo, 'utf-8'); //ler a partir do local (arquivo)
+const dados = carregarDados(bd);
+const alunos = dados.alunos;
+const materias = dados.materias;
+
+//
+function carregarDados(string) { //ler dados...
+    //string = Buffer.from(string, 'base64').toString()
+    const linhas = strings.quebrarString(string, '\n');
+    const primeiraLinha = strings.quebrarString(linhas[0], ';');
+
+    const qtdAlunos = strings.converterStringParaInteiro(primeiraLinha[0]);
+    const qtdMaterias = strings.converterStringParaInteiro(primeiraLinha[1]);
+
+    let aluno;
+    const alunos = [];
+
+    for (let i = 1; i <= qtdAlunos; i++) {
+        aluno = strings.quebrarString(linhas[i], ';');
+        alunos.push(aluno);
+    }
+
+
+    let materia;
+    const materias = [];
+
+    for (let i = (qtdAlunos + 1); i <= (qtdMaterias + qtdAlunos); i++) {
+        materia = strings.quebrarString(linhas[i], ';');
+        materias.push(materia);//adiciona...
+    }
+
+    return {
+        alunos,
+        materias
+    }; // JSON
+}
+
+function prepararDadosSalvar(alunos, materias) { //salva...
+    let dados = `${alunos.length};${materias.length}\n`;
+    
+    for (let i = 0; i < alunos.length; i++) {
+        dados = dados.concat(`${alunos[i]}\n`); //concatena string...
+    }
+
+    for (let i = 0; i < materias.length; i++) {
+        dados = dados.concat(`${materias[i]}\n`); //concatena string...
+    }
+    dados = dados.split(',').join(';');
+    //dados = Buffer.from(dados).toString('base64')
+    return dados;
+}
+
+//modificação da função para uso...
+
+function prepararDadosSalvarAluno(alunos) { //salva...
+    let dados = `${alunos.length}\n`;
+    
+    for (let i = 0; i < alunos.length; i++) {
+        dados = dados.concat(`${alunos[i]}\n`); //concatena string...
+    }
+
+    dados = dados.split(',').join(';');
+    //dados = Buffer.from(dados).toString('base64')
+    return dados;
+}
+function prepararDadosSalvarMateria(materias) { //salva...
+    let dados = `${materias.length}\n`;
+    
+        for (let j = 0; j < materias.length; j++) {
+        dados = dados.concat(`${materias[j]}\n`); //concatena string...
+    }
+    dados = dados.split(',').join(';');
+    //dados = Buffer.from(dados).toString('base64')
+    return dados;
+}
+
+
+//processamento...
+const entrada = terminal.escolherTerminal(); //ler entrada...
+
+let escolhaUsuario = ''; //entrada digitada pelo usuario...
+let mensagem = ''; //escolha a ser exibida...
+for(;escolhaUsuario !== '9';){
+    limparTela(); //função que limpa a tela...
+    exibir(mensagem); //exibe a mensagem (evento ocorrido)
+    menuInicial(); //retorna para o menu...
+    escolhaUsuario = terminal.lerALinhaInteira(entrada); //aguarda uma nova entrada..
+
+    switch (escolhaUsuario) { //swtich (escolha da opção no menu)
+        case '1':
+            var entradaOp = require('readline-sync');
+            var op = entradaOp.question('');
+            console.log(   '\n==================================================================\n \t Menu Alunos\n======================== Digite uma Opção ========================\n\t1 ► Listar Alunos\n\t2 ► Cadastrar\n\t3 ► Retornar ao menu');
+                //switch dentro de outro... kkkk ^.-.^ xD
+                switch(op){
+                    case '1':
+                        console.log('\t Listar Alunos Opção 1 selecionada...\t\n >');
+                        ListaAlunos();
+                        break;
+                    case '2':
+                        AdicionaAluno();
+                    break;
+                    case '3':
+                        //voltar ao menu...
+                        limparTela();
+                        menuInicial();
+                    break;        
+                }
+            //mensagem = menuAlunos();//função menu alunos implementar...
+            break;
+        case '2':
+            //mensagem = Materias(); // implementar...
+            console.log('\t Menu Materias... \n\t 1 > Listar Matéria\n\t 2 > Cadastrar Matéria\n\t 3 > Voltar ao Menu');//implementar...
+            var entradaOp2 = require('readline-sync'); //entrada para menu interno...
+            var op = entradaOp2.question('');//ler menu interno...
+            switch(op){
+                case '1':
+                    console.log('\t Listar Matérias...\t\n >');
+                    ListaMaterias();//alterado.
+                    break;
+                case '2':
+                    AdicionarMatéria();
+                break;
+                case '3':
+                    limparTela();//limpa a tela
+                    menuInicial();//volta para o menu
+
+            }
+            break;
+        case '3':
+            mensagem = LancaNotas(); //implementar...
+            break;
+        case '4':
+            colegio.imprimirBoletim(alunos);//imprime o boletão...
+            mensagem = ''; //retorna a mensagem ...implementar...
+            break;
+        case '9': //encerra e sair...
+            console.log('Saindo...');
+            break;
+        default:
+            mensagem = 'opcao invalida';
+            break;
+    }
+}
+
+
+function menuInicial() {//essa bagaça vai ficar repetitiva, mas f0d4-s3...
+    console.log('==================================================================');
+    console.log('| \t  Cadastro de Alunos Versão 0.1 Beta... \t         |');
+    console.log('========================= Menu Principal =========================\n');
+    console.log(' \t1 ► Alunos\n\t2 ► Materias\n\t3 ► Lançar Notas\n\t4 ► Gerar Resultado Final\n\t9 ► Sair\n\t');
+    console.log('------------------------------------------------------------------');
+    console.log(' \t Digite uma opção acima');
+    console.log('------------------------------------------------------------------');
+}
+
+function ListaAlunos(){
+    const fs = require('fs'); //manipula arquivos... 
+    const localArquivo = './../arquivo/teste.denylson'; //especifica o local e o arquivo...
+
+    const bd = fs.readFileSync(localArquivo, 'utf-8'); //ler a partir do local (arquivo)
+//    const dados = carregarDados(bd);
+//    const alunos = dados.alunos;
+//    mensagem = dados.alunos(alunos); 
+    mensagem = carregarDados(bd); //chegando lá...
+
+}
+function ListaMaterias(materias){
+    const fs = require('fs');
+    const localArquivo = './../arquivo/teste.denylson';
+
+    const bd = fs.readFileSync(localArquivo, 'utf-8');
+//    const dados = carregarDados(bd);
+//    const materias = dados.materias;
+//    mensagem = dados.materias(materias);
+    mensagem = carregarDados(bd); //chegando lá.....
+}
+
+function AdicionaAluno(){
+    var quantidadeparadigitar = require('readline-sync');
+    terminal.imprimir('\n > Informe a quantidde de alunos desejados: \n');//especifica a quantidade a cadastrar...
+    var qtdAlunos = quantidadeparadigitar.question('');//recebe a quantidade desejada...
+    const alunos = []; //array alunos
+    const entrada = terminal.escolherTerminal();//função que vem de terminal captura e trata a entrada
+                        
+        for(let i = 0; i < qtdAlunos; i ++){
+            terminal.imprimir('\n================================================================');
+            terminal.imprimir(' Por favor informe o nome do aluno: ►');
+            terminal.imprimir('=================================================================');
+            let nome = terminal.lerALinhaInteira(entrada);
+                            
+            terminal.imprimir('por favor informe a nota do bimestre 1: ');
+            let nota1 = strings.converterStringParaFloat(terminal.lerALinhaInteira(entrada));
+            terminal.imprimir('\n-----------------------------------------------------------------');
+            terminal.imprimir('por favor informe a nota do bimestre 2: ');
+            let nota2 = strings.converterStringParaFloat(terminal.lerALinhaInteira(entrada));
+            let aluno = [nome, nota1, nota2]; //array 0, 1, 2...
+                            
+            alunos.push(aluno); //adiciona... aluno array...
+            terminal.imprimir('\n-----------------------------------------------------------------');
+            //fins de teste... (backup requerido antes e ao testar...)
+            const dadosPreparados = prepararDadosSalvarAluno(alunos);
+
+            fs.writeFileSync('./../arquivo/teste.denylson', dadosPreparados, 'utf-8'); //apenas salva no arquivo (sobrescreve o que existir)
+
+    }
+}
+
+function AdicionarMatéria(){
+    var quantidadeparadigitar = require('readline-sync');
+    terminal.imprimir('\n > Informe a quantidde de Matérias a cadastrar: \n');
+    var qtdMaterias = quantidadeparadigitar.question('');//recebe a quantidade desejada...
+    const materia = []; //array materias ... alterado
+    const entrada = terminal.escolherTerminal();//função que vem de terminal captura entrada
+                        
+        for(let j = 0; j < qtdMaterias; j ++){
+            terminal.imprimir('\n================================================================');
+            terminal.imprimir(' Por favor informe o nome da Matéria: ►');
+            terminal.imprimir('=================================================================');
+            let nome = terminal.lerALinhaInteira(entrada); //alterado...
+            /* 
+            essa opçao não esta totalmente implementada ou funcional...               
+               */             
+            materia.push(materia); //adiciona materia...
+            terminal.imprimir('\n-----------------------------------------------------------------');
+            //fins de teste... (backup requerido antes e ao testar...)
+            const dadosPreparados = prepararDadosSalvarMateria(materia);
+            fs.writeFileSync('./../arquivo/teste.denylson', dadosPreparados, 'utf-8');//sobrescreve o arquivo... não utilizar...
+
+    }
+}
+
+function limparTela(){
+    console.log('\n\n\n\n\n\n\n\n\n'); //function gambiarristica... >.-.> :v powered by denylson melo...
+}
+
+function LancaNotas(){
+    const fs = require('fs'); //manipula arquivos... 
+    const localArquivo = './../arquivo/teste.denylson'; //especifica o local e o arquivo...
+
+    const bd = fs.readFileSync(localArquivo, 'utf-8'); //ler a partir do local (arquivo)
+
+    colegio.imprimirBoletim(alunos);
+}
+
+function exibir(mensagem){
+    console.log(mensagem);
+}
+
+//salvar no arquivo...
+const dadosPreparados = prepararDadosSalvar(alunos, materias);
+fs.writeFileSync('./../arquivo/teste.denylson', dadosPreparados, 'utf-8');
